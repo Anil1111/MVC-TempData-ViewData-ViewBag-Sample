793514a00d9f2848cd24a276b9b2ca05d54925f3
Krishnanand Sivaraj <SivarajK@advisory.com>
2015-12-07 15:48:15 +0530

    commit #1 - viewdata viewbag tempdata with keep

---
+++ ControllerToViewDataTransfer/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerToViewDataTransfer.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            ViewData["data"] = DateTime.Now.ToString();
+            ViewBag.Text = "My Data here";
+            TempData["key"] = "tempdata value";
+            return View();  //observation -  tempdata is used to persist data upon a request.  When the next request comes, the values disappear and once the tempdata values are used, it is deleted in memory.  There are TempData.Keep and TempData.Peek that comes for survival.
+            //return RedirectToAction("Result");
+        }
+
+        public ActionResult Result()
+        {
+            var data = TempData["key"].ToString();
+            return Content(data);
+        }
+	}
+}
---
+++ ControllerToViewDataTransfer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControllerToViewDataTransfer.MvcApplication" Language="C#" %>
